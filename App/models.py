fa336f95368bed6df3b3de93d9bd370003be7b7c
Name <email@example.ru>
2024-02-23 19:31:51 +0300

    dsds

--- App/models.py
+++ App/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(blank=True, upload_to='uploads/', null=True)
+    bio = models.TextField(blank=True)
+    # Добавление друзей
+    friends = models.ManyToManyField('self', symmetrical=True, related_name='profile_friends')
+
+    def __str__(self):
+        return self.user.username
+    
+class Message(models.Model):
+    sender_id = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    
+    
+    
+    
+    
+
