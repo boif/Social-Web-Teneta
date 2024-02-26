83f330fe56ff700442e95bf11d8f245eb15ae25f
boif3x <boif3x@gmail.com>
2024-02-26 11:04:41 +0300

    layout upd

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,5 +20,6 @@ from App import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('news/', views.news, name='news'),
 ]
