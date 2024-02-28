dc43c8973bc645f1d51b96f5b60ec810a4f79f2d
Name <email@example.ru>
2024-02-29 00:53:45 +0300

    models_image_update

--- App/models.py
+++ App/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=True)
     surname = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(upload_to='media/',blank=True,null=True)
     bio = models.TextField(blank=True)
     age = models.IntegerField(null=True)
     friends = models.ManyToManyField('self',symmetrical=True,blank=True)
         
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from App import views
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -28,4 +30,4 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
