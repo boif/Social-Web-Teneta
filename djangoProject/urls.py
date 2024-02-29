a1ebd60fa0e7dc21394eb1256207fb00d4438f54
boif <chlentim7@gmail.com>
2024-02-29 21:39:11 +0300

    clear

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -23,7 +23,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', views.news, name='news'),
     path('profile/',views.profile_view,name='profile_view'),
     path("accounts/", include("django.contrib.auth.urls")),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
