c4c47d22dc45884b1fdd6a5e7dbc79c75bf97a74
boif <chlentim7@gmail.com>
2024-03-03 17:30:47 +0300

    create post + home.html

--- News/forms.py
+++ News/forms.py
@@ -4,4 +4,4 @@ from News.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['author', 'text', 'content']
+        fields = ['text', 'content']
