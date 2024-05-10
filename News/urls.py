5609080800c5b455e495d928ed2de8f9740af274
boif <chlentim7@gmail.com>
2024-05-10 19:12:52 +0300

    profile upd

--- News/urls.py
+++ News/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('', views.createPost, name='home'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post'),
-    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-edit'),
-    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post_delete'),
     path('like/<int:post_id>/', views.like_post, name='like_post'),
     path('comment/<int:post_id>/', views.add_comment, name='add_comment'),
 ]
